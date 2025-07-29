617ade598ddf59a5ce4f8713099a99ef98791d55
hprob5 <95711518+hprob5@users.noreply.github.com>
2025-07-29 00:07:05 -0400

    Final

--- API.R
+++ API.R
@@ -12,6 +12,9 @@ final_model_api <- readRDS("final_model.rds")
 # read in data that we need
 diabetes_data_api <- readRDS("diabetes_data.rds")
 
+# using the best model fit on the full data set to make predictions
+overall_preds <- predict(final_model_api, new_data = diabetes_data_api, type = "prob")
+
 #* @apiTitle ST 558 Project 3
 #* @apiDescription This API will work with the best fit model found in the Modeling.qmd file
 
@@ -23,7 +26,7 @@ diabetes_data_api <- readRDS("diabetes_data.rds")
 #* @param HeartDA no or yes
 #* @param Sex M or F
 #* @param Smoking_status no or yes
-#* @post /pred
+#* @get /pred
 function(Health_level = "verygood", BP_lvl = "no", BMI = 28.38, Chol_lvl  = "no",
          HeartDA = "no", Sex = "F", Smoking_status = "no") {
   
@@ -52,5 +55,9 @@ function() {
 }
 
 # 3 example URLs
+# http://localhost:8000/pred?Health_level=verygood&BP_lvl=no&BMI=28.38&Chol_lvl=no&HeartDA=no&Sex=F&Smoking_status=no
 
+# http://localhost:8000/pred?Health_level=excellent&BP_lvl=no&BMI=28.38&Chol_lvl=no&HeartDA=no&Sex=M&Smoking_status=yes
+
+# http://localhost:8000/pred?Health_level=good&BP_lvl=high&BMI=25&Chol_lvl=no&HeartDA=no&Sex=F&Smoking_status=no
 
